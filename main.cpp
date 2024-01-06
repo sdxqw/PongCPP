a8ba33fcf34bc91463f3cb844ac613ab17934a1b
sdxq <xntrowdev@gmail.com>
2024-01-06 16:29:39 +0100

    init

--- main.cpp
+++ main.cpp
@@ -1,53 +1,40 @@
-
 #include <iostream>
 #include <random>
-#include <bits/random.h>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
-constexpr u_int16_t PlayerWidth = 16;
-constexpr u_int16_t PlayerHeight = 64;
-constexpr u_int16_t BallRadius = 16;
+constexpr uint16_t PlayerWidth = 16;
+constexpr uint16_t PlayerHeight = 64;
+constexpr uint16_t BallRadius = 16;
 constexpr float BallSpeed = 320.0f;
 constexpr int MaxScore = 5;
 constexpr int WindowWidth = 1280;
 constexpr int WindowHeight = 720;
 constexpr char FontPath[] = "assets/Minecraft.ttf";
 
+uint8_t PlayerScore = 0;
+uint8_t EnemyScore = 0;
 
-class Score {
-public:
-    Score() : score(0), maxScore(MaxScore) {
-    }
-
-    [[nodiscard]] u_int8_t get() const { return score; }
-    [[nodiscard]] bool isMax() const { return score == maxScore; }
-    void update() { score++; }
-    void reset() { score = 0; }
-
-private:
-    u_int8_t score;
-    u_int8_t maxScore;
-};
+enum class SceneType { MainMenu, Game, GameOver };
 
 class Entity {
 public:
-    Entity(u_int16_t x, u_int16_t y, u_int16_t width, u_int16_t height)
+    Entity(uint16_t x, uint16_t y, uint16_t width, const uint16_t height)
         : shape(sf::Vector2f(width, height)), x(x), y(y) {
         shape.setPosition(sf::Vector2f(x, y));
     }
 
-    void update(float dt) {
+    void updatePlayer(const float dt) {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && shape.getPosition().y > 0)
             shape.move(0, -BallSpeed * dt * 2);
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && shape.getPosition().y + shape.getSize().y < WindowHeight)
             shape.move(0, BallSpeed * dt * 2);
     }
 
-    void update(float dt, const sf::CircleShape &ballShape) {
+    void updateBot(const float dt, const sf::CircleShape &ballShape) {
         std::random_device rd;
         std::mt19937 gen(rd());
-        std::uniform_real_distribution dis(-1.0f, 1.0f);
+        std::uniform_real_distribution<float> dis(-1.0f, 1.0f);
 
         const float ballY = ballShape.getPosition().y + ballShape.getRadius();
         const float enemyY = shape.getPosition().y + shape.getSize().y / 2;
@@ -64,23 +51,19 @@ public:
         }
     }
 
-
-    void render(sf::RenderWindow &window) const { window.draw(shape); }
     void reset() { shape.setPosition(x, y); }
-    sf::RectangleShape &get() { return shape; }
+    sf::RectangleShape &getShape() { return shape; }
 
 private:
     sf::RectangleShape shape;
-    u_int16_t x;
-    u_int16_t y;
+    uint16_t x;
+    uint16_t y;
 };
 
 class Ball {
 public:
     Ball(Entity &player, Entity &enemy) : player(player), enemy(enemy), shape(BallRadius), dX(1), dY(1) {
-        reset();
-        scorePlayer = Score();
-        scoreEnemy = Score();
+        shape.setPosition(static_cast<float>(WindowWidth) / 2, static_cast<float>(WindowHeight) / 2);
     }
 
     void update(float dt) {
@@ -89,225 +72,88 @@ public:
         shape.move(BallSpeed * 2 * dX * dt, BallSpeed * 2 * dY * dt);
 
         if (shape.getPosition().x < 0) {
-            scoreEnemy.update();
+            EnemyScore++;
             reset();
         }
         if (shape.getPosition().x + shape.getRadius() * 2 > WindowWidth) {
-            scorePlayer.update();
+            PlayerScore++;
             reset();
         }
-        if (scorePlayer.isMax() || scoreEnemy.isMax()) {
+        if (PlayerScore == MaxScore || EnemyScore == MaxScore) {
             player.reset();
             enemy.reset();
         }
     }
 
-    void render(sf::RenderWindow &window) const { window.draw(shape); }
-
     bool isColliding(Entity &entity) const {
-        return shape.getGlobalBounds().intersects(entity.get().getGlobalBounds());
+        return shape.getGlobalBounds().intersects(entity.getShape().getGlobalBounds());
     }
 
     void reset() {
         shape.setPosition(static_cast<float>(WindowWidth) / 2, static_cast<float>(WindowHeight) / 2);
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        dX = std::uniform_real_distribution(1.0f, 1.0f)(gen);
-        dY = std::uniform_real_distribution(1.0f, 1.0f)(gen);
     }
 
-    Score &getScorePlayer() { return scorePlayer; }
-    Score &getScoreEnemy() { return scoreEnemy; }
-
-    sf::CircleShape &get() { return shape; }
+    sf::CircleShape &getShape() { return shape; }
 
 private:
     Entity &player;
     Entity &enemy;
-    Score scorePlayer;
-    Score scoreEnemy;
     sf::CircleShape shape;
     float dX;
     float dY;
 };
 
-class SceneManager {
-public:
-    enum class SceneType { MainMenu, Game, GameOver };
+void drawText(const sf::Font &font, uint8_t size, const std::string &textToDisplay, float x, float y,
+              sf::RenderWindow &window) {
+    sf::Text text;
+    text.setFont(font);
+    text.setString(textToDisplay);
+    text.setCharacterSize(size);
+    text.setFillColor(sf::Color::White);
 
-    SceneManager(Entity &player, Entity &enemy, Ball &ball, sf::RenderWindow &window) : player(player), enemy(enemy),
-        ball(ball), window(window) {
-        if (!font.loadFromFile(FontPath)) std::cout << "Error loading font" << std::endl;
+    const sf::FloatRect textRect = text.getLocalBounds();
+    text.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
+    text.setPosition(x, y);
+
+    window.draw(text);
+}
+
+void renderMainMenu(sf::RenderWindow &window, const sf::Font &font) {
+    drawText(font, 64, "Pong!!!", static_cast<float>(WindowWidth) / 2.0f, static_cast<float>(WindowHeight) / 2.0f,
+             window);
+    drawText(font, 32, "Press Space to start | Q to quit", static_cast<float>(WindowWidth) / 2.0f,
+             static_cast<float>(WindowHeight) / 2.0f + 60, window);
+}
+
+void renderGame(sf::RenderWindow &window, Entity &player, Entity &enemy, Ball &ball, const sf::Font &font) {
+    drawText(font, 32, std::to_string(PlayerScore), static_cast<float>(WindowWidth) / 2.0f - 100, 10, window);
+    drawText(font, 32, std::to_string(EnemyScore), static_cast<float>(WindowWidth) / 2.0f + 100, 10, window);
+    window.draw(player.getShape());
+    window.draw(enemy.getShape());
+    window.draw(ball.getShape());
+}
+
+void renderGameOver(sf::RenderWindow &window, const sf::Font &font) {
+    std::string winnerText;
+    if (PlayerScore == MaxScore) {
+        winnerText = "Winner: Player - " + std::to_string(PlayerScore);
+    } else if (EnemyScore == MaxScore) {
+        winnerText = "Winner: Enemy - " + std::to_string(EnemyScore);
     }
 
-    void update(float dt) {
-        switch (currentScene) {
-            case SceneType::MainMenu:
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-                    reset();
-                    this->currentScene = SceneType::Game;
-                }
+    drawText(font, 64, winnerText, static_cast<float>(WindowWidth) / 2.0f, static_cast<float>(WindowHeight) / 2.0f,
+             window);
+    drawText(font, 32, "Press Space to restart | Q to quit", static_cast<float>(WindowWidth) / 2.0f,
+             static_cast<float>(WindowHeight) / 2.0f + 60, window);
+}
 
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
-                    window.close();
-                break;
-            case SceneType::Game:
-                player.update(dt);
-                enemy.update(dt, ball.get());
-                ball.update(dt);
-
-                if (ball.getScoreEnemy().isMax() || ball.getScorePlayer().isMax()) {
-                    this->currentScene = SceneType::GameOver;
-                }
-
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-                    reset();
-                    this->currentScene = SceneType::MainMenu;
-                }
-
-                break;
-            case SceneType::GameOver:
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
-                    reset();
-                    this->currentScene = SceneType::Game;
-                }
-
-                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-                    reset();
-                    this->currentScene = SceneType::MainMenu;
-                }
-
-                break;
-        }
-    }
-
-    void render() {
-        switch (currentScene) {
-            case SceneType::MainMenu:
-                renderMainMenu();
-                break;
-            case SceneType::Game:
-                player.render(window);
-                enemy.render(window);
-                ball.render(window);
-                renderScore();
-                break;
-            case SceneType::GameOver:
-                renderGameOver();
-                break;
-        }
-    }
-
-private:
-    SceneType currentScene = SceneType::MainMenu;
-    Entity &player;
-    Entity &enemy;
-    Ball &ball;
-    sf::Font font;
-    sf::RenderWindow &window;
-    int blinkCounter = 1000;
-
-    void reset() const {
-        player.reset();
-        enemy.reset();
-        ball.reset();
-        ball.getScoreEnemy().reset();
-        ball.getScorePlayer().reset();
-    }
-
-    void renderScore() const {
-        sf::Text scorePlayerText;
-        sf::Text scoreEnemyText;
-        scorePlayerText.setFont(font);
-        scoreEnemyText.setFont(font);
-        scorePlayerText.setString(std::to_string(ball.getScorePlayer().get()));
-        scoreEnemyText.setString(std::to_string(ball.getScoreEnemy().get()));
-        scorePlayerText.setCharacterSize(64);
-        scoreEnemyText.setCharacterSize(64);
-        scorePlayerText.setFillColor(sf::Color::White);
-        scoreEnemyText.setFillColor(sf::Color::White);
-
-        scoreEnemyText.setPosition(static_cast<float>(WindowWidth) / 2 + 64, 0);
-        scorePlayerText.setPosition(static_cast<float>(WindowWidth) / 2 - 64, 0);
-
-        window.draw(scorePlayerText);
-        window.draw(scoreEnemyText);
-    }
-
-    void renderGameOver() {
-        sf::Text gameOverText;
-        sf::Text blinkyText;
-        gameOverText.setFont(font);
-        blinkyText.setFont(font);
-
-        if (ball.getScorePlayer().isMax()) {
-            gameOverText.setString("Winner: Player - " + std::to_string(ball.getScorePlayer().get()));
-        } else if (ball.getScoreEnemy().isMax()) {
-            gameOverText.setString("Winner: Enemy - " + std::to_string(ball.getScoreEnemy().get()));
-        }
-
-        gameOverText.setCharacterSize(64);
-        gameOverText.setFillColor(sf::Color::White);
-
-        blinkyText.setString("Press Space to restart | Q to quit");
-        blinkyText.setCharacterSize(32);
-        blinkyText.setFillColor(sf::Color::White);
-        blinkyText.setPosition(static_cast<float>(WindowWidth) / 2.0f - 250,
-                               static_cast<float>(WindowHeight) / 2.0f + 40);
-
-        sf::FloatRect textRect = gameOverText.getLocalBounds();
-        gameOverText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
-        gameOverText.setPosition(static_cast<float>(WindowWidth) / 2.0f, static_cast<float>(WindowHeight) / 2.0f);
-
-        window.draw(gameOverText);
-
-        blinkCounter++;
-        if (blinkCounter < 2000) {
-            blinkyText.setFillColor(sf::Color(255 - blinkCounter / 8, 255 - blinkCounter / 8, 255 - blinkCounter / 8));
-        } else if (blinkCounter >= 2500) {
-            blinkCounter = 1000;
-        }
-
-        window.draw(blinkyText);
-    }
-
-    void renderMainMenu() {
-        sf::Text titleText;
-        sf::Text blinkyText;
-
-        titleText.setFont(font);
-        blinkyText.setFont(font);
-
-        titleText.setString("Pong!!!");
-        titleText.setCharacterSize(64);
-        titleText.setFillColor(sf::Color::White);
-
-        blinkyText.setString("Press Space to start | Q to quit");
-        blinkyText.setCharacterSize(32);
-        blinkyText.setFillColor(sf::Color::White);
-
-
-        sf::FloatRect textRect = titleText.getLocalBounds();
-        titleText.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
-        titleText.setPosition(static_cast<float>(WindowWidth) / 2.0f, static_cast<float>(WindowHeight) / 2.0f);
-
-        blinkyText.setPosition(static_cast<float>(WindowWidth) / 2.0f - 220,
-                               static_cast<float>(WindowHeight) / 2.0f + 40);
-
-
-        window.draw(titleText);
-
-
-        blinkCounter++;
-        if (blinkCounter < 2000) {
-            blinkyText.setFillColor(sf::Color(255 - blinkCounter / 8, 255 - blinkCounter / 8, 255 - blinkCounter / 8));
-        } else if (blinkCounter >= 2500) {
-            blinkCounter = 1000;
-        }
-
-        window.draw(blinkyText);
-    }
-};
+void reset(Entity &player, Entity &enemy, Ball &ball) {
+    PlayerScore = 0;
+    EnemyScore = 0;
+    player.reset();
+    enemy.reset();
+    ball.reset();
+}
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(WindowWidth, WindowHeight), "Pong!!!",
@@ -316,18 +162,41 @@ int main() {
     Entity enemy(window.getSize().x - PlayerWidth - 10, window.getSize().y / 2 - PlayerWidth, PlayerWidth,
                  PlayerHeight);
     Ball ball(player, enemy);
-    SceneManager sceneManager(player, enemy, ball, window);
+    sf::Font font;
+    if (!font.loadFromFile(FontPath)) std::cout << "Error loading font" << std::endl;
 
     sf::Clock clock;
+    SceneType currentScene = SceneType::MainMenu;
     while (window.isOpen()) {
         const float dt = clock.restart().asSeconds();
+
         sf::Event event{};
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) window.close();
         }
-        sceneManager.update(dt);
+
+        if (PlayerScore == MaxScore || EnemyScore == MaxScore) currentScene = SceneType::GameOver;
+
         window.clear();
-        sceneManager.render();
+        switch (currentScene) {
+            case SceneType::MainMenu:
+                reset(player, enemy, ball);
+                renderMainMenu(window, font);
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) currentScene = SceneType::Game;
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) window.close();
+                break;
+            case SceneType::Game:
+                renderGame(window, player, enemy, ball, font);
+                enemy.updateBot(dt, ball.getShape());
+                player.updatePlayer(dt);
+                ball.update(dt);
+                break;
+            case SceneType::GameOver:
+                renderGameOver(window, font);
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) currentScene = SceneType::Game;
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) currentScene = SceneType::MainMenu;
+                break;
+        }
         window.display();
     }
     return EXIT_SUCCESS;
